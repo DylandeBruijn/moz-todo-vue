4073707dc8ae2477d0e002cc764734c2770236e0
Dylan de Bruijn <dylan@bendingtherules.nl>
2023-01-30 15:43:26 +0100

    Add to do list

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
 <template>
   <h1>To-Do List</h1>
   <ul>
-    <li>
-      <to-do-item label="My ToDo Item" :done="false"></to-do-item>
+    <li v-for="toDoItem in ToDoItems" :key="toDoItem.id">
+      <to-do-item 
+        :label="toDoItem.label"
+        :done="toDoItem.done" 
+        :id="toDoItem.id"
+      ></to-do-item>
     </li>
   </ul>
 </template>
 
 <script>
-  import ToDoItem from "./components/ToDoItem.vue";
+import ToDoItem from "./components/ToDoItem.vue";
+import uniqueId from "lodash.uniqueid";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     ToDoItem
+  },
+  data() {
+    return {
+      ToDoItems: [
+        { id: uniqueId("todo-"), label: "Learn Vue", done: false },
+        {
+          id: uniqueId("todo-"),
+          label: "Create a Vue project with the CLI",
+          done: true
+        },
+        { id: uniqueId("todo-"), label: "Have fun", done: true },
+        { id: uniqueId("todo-"), label: "Create a to-do list", done: false }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  ul {
+    list-style-type: none;
+  }
 </style>
